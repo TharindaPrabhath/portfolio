b525816d6c07067300dffeb9ab87c5bcefbf7a02
TharindaPrabhath <tharindahp@gmail.com>
2024-01-02 02:24:58 +0530

    changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,11 +8,20 @@ import { Menu } from "lucide-react";
 
 function Header() {
   return (
-    <header className="container flex flex-row items-center justify-between py-4">
+    <header className="container flex flex-row items-center justify-between py-4 md:py-8">
       <Link href="/">
-        <Image src={"/logo.svg"} alt="Logo" width={150} height={80} />
+        <Image src="/logo.svg" alt="Logo" width={150} height={80} />
       </Link>
-      <Button variant="ghost" size="icon">
+
+      <nav className="hidden flex-row items-center gap-12 md:flex">
+        {Tabs.map((tab) => (
+          <Link key={tab.name} href={tab.href}>
+            <p>{tab.name}</p>
+          </Link>
+        ))}
+      </nav>
+
+      <Button variant="ghost" size="icon" className="md:hidden">
         <Menu className="w-8 h-8" />
       </Button>
     </header>
@@ -20,3 +29,26 @@ function Header() {
 }
 
 export default Header;
+
+const Tabs = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Work",
+    href: "/work",
+  },
+  {
+    name: "Education",
+    href: "/education",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
